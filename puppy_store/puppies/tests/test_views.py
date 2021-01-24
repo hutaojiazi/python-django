cc16256ff361176116f0a282042a8388c4d3a0b4
i514097 <tianli.gao@sap.com>
2021-01-24 16:03:53 -0500

    refactoring

--- puppy_store/puppies/tests/test_views.py
+++ puppy_store/puppies/tests/test_views.py
@@ -2,8 +2,8 @@ import json
 from rest_framework import status
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Puppy
-from .serializers import PuppySerializer
+from ..models import Puppy
+from ..serializers import PuppySerializer
 
 
 # initialize the APIClient app
